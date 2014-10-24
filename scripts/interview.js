404675d59ed38afd7dcf5968366e6ffcb31fe255
Richa Avasthi <richa@outright.com>
2014-10-24 08:41:43 -0700

    Add JS.

---
+++ scripts/interview.js
@@ -0,0 +1,62 @@
+/*------------------------------------------------------------------------------
+| scratch.js
+| application name v1.0
+|
+| file description.
+------------------------------------------------------------------------------*/
+
+var results = [
+  {
+    url: "images/search-results/result-01.jpg",
+    title: "Title 01",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-02.jpg",
+    title: "Title 02",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-03.jpg",
+    title: "Title 03",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-04.jpg",
+    title: "Title 04",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-05.jpg",
+    title: "Title 05",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-06.jpg",
+    title: "Title 06",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-07.jpg",
+    title: "Title 07",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-08.jpg",
+    title: "Title 08",
+    date: "21 Sep 2014"
+  },
+  {
+    url: "images/search-results/result-09.jpg",
+    title: "Title 09",
+    date: "21 Sep 2014"
+  }
+];
+
+(function($) {
+  $(function() {
+    // Insert your JS here
+  });
+})(jQuery);
+
+
