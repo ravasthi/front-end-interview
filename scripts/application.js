0af8f30023fbfcd5ab613d40e8510b70d684d76e
Richa Avasthi <ravasthi@gmail.com>
2015-02-08 21:58:26 -0800

    Wire up form to flickr api.

---
+++ scripts/application.js
@@ -0,0 +1,6 @@
+//= require vendor/jquery
+//= require vendor/jquery-ui.widget
+//= require vendor/fiftyfive-utils/feature-detect
+//= require common
+//= require photo-search
+//= require interview
---
+++ scripts/photo-search.js
@@ -0,0 +1,141 @@
+/*------------------------------------------------------------------------------
+| photo-search.js
+|
+| Utilities for hooking up the search box to the Flickr API
+------------------------------------------------------------------------------*/
+var FrontEndInterview = window.FrontEndInterview ? window.FrontEndInterview : {};
+
+// function called by eval-ing response from Flickr
+function jsonFlickrApi(response) {
+  if(response.stat == 'ok') {
+    FrontEndInterview.photoSearch.populateResults(response);
+  }
+}
+
+FrontEndInterview.photoSearch = {
+  flickrApiKey:          '7b491816b7ce18f1f848e52962352ee7',
+  flickrRestUrlBase:     'https://www.flickr.com/services/rest/?',
+  flickrMaxCountPerPage: 500,
+  searchText:            '',
+
+  utils: {
+    sanitizeInput: function(string) {
+      danger = [
+        /<script[^>]*?>.*?<\/script>/gi, // Strip out javascript
+        /<style[^>]*?>.*?<\/style>/gi,   // Strip style tags
+        /<![\s\S]*?--[ \t\n\r]*>/gi,     // Strip multi-line comments
+        /<[\/\!]*?[^<>]*?>/gi            // Strip out HTML tags
+      ];
+
+      for(var i = 0; i < danger.length; i++) {
+        string = string.replace(danger[i], '');
+      }
+
+      // Also collapse multiple spaces down to one
+      string = string.replace(/\s+/, ' ');
+
+      return string;
+    },
+
+    buildFlickrOptions: function(options) {
+      options         = options ? options : {};
+
+      options.format  = 'json';
+      options.api_key = FrontEndInterview.photoSearch.flickrApiKey;
+
+      return options;
+    },
+
+    buildFlickrPhotoUrl: function(photo) {
+      /*
+      ** Flickr photo urls take the following format:
+      **
+      ** https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_{size}.jpg
+      */
+
+      return "https://farm" + photo.farm +
+             ".staticflickr.com/" + photo.server + "/" +
+             photo.id + "_" + photo.secret + "_" + "c.jpg";
+    }
+  },
+
+  toggleSearching: function() {
+    FrontEndInterview.photoSearch.$searchButton.find(".search-icon").toggle();
+    FrontEndInterview.photoSearch.$searchButton.find(".spinner").toggle();
+  },
+
+  getImages: function(searchText, maxCount) {
+    FrontEndInterview.photoSearch.toggleSearching();
+
+    if(maxCount && (maxCount * 1) > FrontEndInterview.photoSearch.flickrMaxCountPerPage) {
+      maxCount = flickrMaxCountPerPage;
+    }
+
+    if(searchText && typeof searchText == 'string') {
+      FrontEndInterview.photoSearch.searchText = searchText;
+    }
+
+    var flickrOptions = {
+      method:       'flickr.photos.search',
+      text:         searchText ? searchText : FrontEndInterview.photoSearch.searchText,
+      page:         1,
+      per_page:     maxCount ? maxCount : FrontEndInterview.photoSearch.flickrMaxCountPerPage,
+      media:        'photos',
+      content_type: 1
+    };
+
+    flickrOptions = FrontEndInterview.photoSearch.utils.buildFlickrOptions(flickrOptions);
+
+    var url = FrontEndInterview.photoSearch.flickrRestUrlBase;
+
+    url += $.param(flickrOptions);
+
+    $.ajax(url, {
+      success: function(data, status, xhr) {
+        FrontEndInterview.photoSearch.$alerts.html('');
+        FrontEndInterview.photoSearch.$searchString.text(FrontEndInterview.photoSearch.searchText);
+        eval(xhr.responseText);
+      },
+      error: function(data, status, xhr) {
+        FrontEndInterview.photoSearch.$alerts.append(
+          '<div class="message error">Something went wrong…</div>'
+        );
+      },
+      complete: function(xhr, status) {
+        FrontEndInterview.photoSearch.toggleSearching();
+      }
+    });
+  },
+
+  checkEnter: function(event) {
+    var characterCode = event.keyCode;
+
+    if(characterCode == 13) { // if enter key
+      FrontEndInterview.photoSearch.doSearch();
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+  doSearch: function(event) {
+    searchKeywords = FrontEndInterview.photoSearch.utils.sanitizeInput(
+      FrontEndInterview.photoSearch.$searchField.val()
+    );
+
+    if(searchKeywords.match(/^\s*$/) == null) {
+      FrontEndInterview.photoSearch.searchText = searchKeywords;
+      FrontEndInterview.photoSearch.getImages(searchKeywords);
+    }
+  },
+
+  init: function() {
+    this.$searchField  = $(".search-field");
+    this.$searchButton = $("button.search");
+    this.$searchString = $(".search-string");
+    this.$alerts       = $(".alerts");
+
+    this.$searchButton.on('click', this.doSearch);
+    this.$searchField.on('keypress', this.checkEnter);
+  }
+};
